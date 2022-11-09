819c599ed833ae19a8454cfac0aab0bf2670788f
Wilfredo Palencia <wpalencia@italbank.com>
2022-11-09 12:09:28 -0400

    archvo mod wapalencia

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/wapalencia/algoritmo/internal/"
+)
+
+func main() {
+
+	var values = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	var LeerArray = []string{"1", "m", "u", "y", "-", "f", "a", "c", "i", "l", "9", "0"}
+	internal.ValidarPar(values)
+	fmt.Println(internal.RecorrerPares(-5, 6, 2))
+	fmt.Println(internal.EncontrarPalabra(LeerArray))
+}
